b07a71b7034bb4bc8dcbec705fff25bfffad77a4
Cobr3x <151967586+Cobr3x@users.noreply.github.com>
2024-01-15 22:00:35 +0100

    Add files via upload

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,65 +1,56 @@
 #include "Game.h"
 
-namespace monopoly
+namespace monopoly   	
 {
 	bool random_probability(unsigned int probability_percent)
 	{
 		unsigned int generated_number = rand() % probability_percent;
-		if (generated_number < probability_percent)
+		if(generated_number < probability_percent)
 		{
-			return true;
+		    return true;
 		}
 		return false;
 	}
-
-	unsigned int roll(unsigned int n_dice, unsigned int max_value)
+	    
+	unsigned int roll(unsigned int n_dice, unsigned int max_value) 
 	{
 		unsigned int value_roll;
-
-		for (int i = 0; i < n_dice; i++)
+		
+		for(int i=0; i < n_dice; i++)
 		{
 			value_roll += 1 + (rand() % max_value);
 		}
-
+		
 		return value_roll;
 	}
 
-	void play_bot(GameBoard &current_gameboard, Player *player)
+	void play_bot(GameBoard& current_gameboard, Player* player)
 	{
 		std::srand(std::time(NULL));
-		player->set_last_roll(roll(2, 6));
-		Log::log_dice(player->get_name(), player->get_last_roll());
-
+		player->set_last_roll(roll(2,6));
+		
 		if (player->get_current_position() + player->get_last_roll() >= GameBoard::MAX_SIZE)
 		{
 			Box *start_box = current_gameboard.get_box(GameBoard::START);
-			player->set_balance(player->get_balance() + dynamic_cast<AngularBox *>(start_box)->start_bonus());
-			Log::log_start(player->get_name(), dynamic_cast<AngularBox *>(start_box)->start_bonus());
+			player->set_balance( player->get_balance() + dynamic_cast<AngularBox *>(start_box)->start_bonus());
 		}
 
 		player->set_current_position((player->get_current_position() + player->get_last_roll()) % GameBoard::MAX_SIZE);
-		Log::log_advance(player->get_name(), player->get_current_position());
 
 		Box *current_box = current_gameboard.get_box(player->get_current_position());
 
 		if (!dynamic_cast<LateralBox *>(current_box))
 		{
-			Log::log_game_over(player->get_name());
 			return;
 		}
 
 		if (dynamic_cast<LateralBox *>(current_box)->is_for_sale())
 		{
 
-			if (random_probability(GameBoard::BOT_ACTION_PROBABILITY))
+			if(random_probability(GameBoard::BOT_ACTION_PROBABILITY))
 			{
-				if (dynamic_cast<LateralBox *>(current_box)->sell(player))
-				{
-					Log::log_sold(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_name());
-				}
+				dynamic_cast<LateralBox *>(current_box)->sell(player);
 			}
-			Log::log_game_over(player->get_name());
-
 			return;
 		}
 
@@ -68,56 +59,43 @@ namespace monopoly
 			if (!dynamic_cast<LateralBox *>(current_box)->stay(player))
 			{
 				player->set_in_game(false);
-				Log::log_game_over(player->get_name());
-				Log::log_loser(player->get_name());
-				return;
 			}
-			Log::log_overnight_stay(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_owner()->get_name(), dynamic_cast<LateralBox *>(current_box)->get_price((int)dynamic_cast<LateralBox *>(current_box)->get_category()), dynamic_cast<LateralBox *>(current_box)->get_name());
-			Log::log_game_over(player->get_name());
-
 			return;
 		}
+
 
 		if (!dynamic_cast<LateralBox *>(current_box)->has_last_upgrade())
 		{
 			LateralBox::Building building = dynamic_cast<LateralBox *>(current_box)->get_building();
 
-			if (random_probability(GameBoard::BOT_ACTION_PROBABILITY))
+			if(random_probability(GameBoard::BOT_ACTION_PROBABILITY))
 			{
-				if (!dynamic_cast<LateralBox *>(current_box)->upgrade())
+				if(dynamic_cast<LateralBox *>(current_box)->upgrade())
 				{
-					std::cout << "You do not have enough money." << std::endl;
-					Log::log_game_over(player->get_name());
-					return;
+					std::cout << "You do not have enough money." << std::endl; 				
 				}
-				Log::log_upgrade(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_name());
 			}
+			return;
 		}
-		Log::log_game_over(player->get_name());
 	}
 
-	void play_human(GameBoard &current_gameboard, Player *player)
+	void play_human(GameBoard& current_gameboard, Player* player)
 	{
 		std::srand(std::time(NULL));
-		player->set_last_roll(roll(2, 6));
-		Log::log_dice(player->get_name(), player->get_last_roll());
-
+		player->set_last_roll(roll(2,6));
+		
 		if (player->get_current_position() + player->get_last_roll() >= GameBoard::MAX_SIZE)
 		{
 			Box *start_box = current_gameboard.get_box(GameBoard::START);
-			player->set_balance(player->get_balance() + dynamic_cast<AngularBox *>(start_box)->start_bonus());
-			Log::log_start(player->get_name(), dynamic_cast<AngularBox *>(start_box)->start_bonus());
+			player->set_balance( player->get_balance() + dynamic_cast<AngularBox *>(start_box)->start_bonus());
 		}
-
+		
 		player->set_current_position((player->get_current_position() + player->get_last_roll()) % GameBoard::MAX_SIZE);
-		Log::log_advance(player->get_name(), player->get_current_position());
 
 		Box *current_box = current_gameboard.get_box(player->get_current_position());
 
 		if (!dynamic_cast<LateralBox *>(current_box))
 		{
-			Log::log_game_over(player->get_name());
-
 			return;
 		}
 
@@ -127,19 +105,22 @@ namespace monopoly
 
 			do
 			{
+				std::cout << std::endl;
+				std::cout << "Print S to use command < show >" << std::endl;
 				std::cout << "Do you want to buy the property? (Y / N):";
 				std::cin >> response;
 				std::cout << std::endl;
+				if(toupper(response) == 'S')
+				{
+					std::system("clear");
+					current_gameboard.show();
+				}
 			} while (toupper(response) != 'Y' && toupper(response) != 'N');
 
-			if (response == 'Y')
+			if(response == 'Y')
 			{
-				if (dynamic_cast<LateralBox *>(current_box)->sell(player))
-				{
-					Log::log_sold(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_name());
-				}
+				dynamic_cast<LateralBox *>(current_box)->sell(player);
 			}
-			Log::log_game_over(player->get_name());
 			return;
 		}
 
@@ -148,13 +129,7 @@ namespace monopoly
 			if (!dynamic_cast<LateralBox *>(current_box)->stay(player))
 			{
 				player->set_in_game(false);
-				Log::log_game_over(player->get_name());
-				Log::log_loser(player->get_name());
-				return;
 			}
-			Log::log_overnight_stay(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_owner()->get_name(), dynamic_cast<LateralBox *>(current_box)->get_price((int)dynamic_cast<LateralBox *>(current_box)->get_category()), dynamic_cast<LateralBox *>(current_box)->get_name());
-			Log::log_game_over(player->get_name());
-
 			return;
 		}
 
@@ -165,25 +140,28 @@ namespace monopoly
 
 			do
 			{
-				std::cout << "Do you want to upgrade your property? (Y / N):";
+				std::cout << std::endl;
+				std::cout << "Print S to use command < show >" << std::endl;
+				std::cout << "Do you want to buy the property? (Y / N):";
 				std::cin >> response;
 				std::cout << std::endl;
+				if(toupper(response) == 'S')
+				{
+					std::system("clear");
+					current_gameboard.show();
+				}
 			} while (toupper(response) != 'Y' && toupper(response) != 'N');
 
-			if (response == 'Y')
+			if(response == 'Y')
 			{
-				if (!dynamic_cast<LateralBox *>(current_box)->upgrade())
+				if(dynamic_cast<LateralBox *>(current_box)->upgrade())
 				{
-					std::cout << "You do not have enough money." << std::endl;
-					Log::log_game_over(player->get_name());
-					return;
+					std::cout << "You do not have enough money." << std::endl; 				
 				}
-				Log::log_upgrade(player->get_name(), dynamic_cast<LateralBox *>(current_box)->get_name());
 			}
+			return;
 		}
-		Log::log_game_over(player->get_name());
-	}
-
+	}	
 	
 	std::vector<int> player_order()
 	{
@@ -213,3 +191,4 @@ namespace monopoly
 		return order;
 	}
 }
+
