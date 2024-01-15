a25bfa2d55bb2160e772706d0f93b20661d3d7c7
Cobr3x <151967586+Cobr3x@users.noreply.github.com>
2024-01-15 18:57:03 +0100

    Update Game.h

--- include/Game.h
+++ include/Game.h
@@ -8,17 +8,20 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
 
 namespace monopoly   	
 {
 	bool random_probability(unsigned int probability_percent);
     
-	unsigned int roll();
+	unsigned int roll(unsigned int n_dice, unsigned int max_value);
 
 	void play_bot(GameBoard& current_gameboard, Player* player);
 
 	void play_human(GameBoard& current_gameboard, Player* player);
+	
+	std::vector<int> player_order();
 			
 }
 
