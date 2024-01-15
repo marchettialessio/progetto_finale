9c0f73b39a6fade9474b526f2047d96d1a6c2d3e
Cobr3x <151967586+Cobr3x@users.noreply.github.com>
2024-01-15 21:54:59 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,72 @@ using namespace monopoly;
 
 int main(int argc, char *argv[])
 {
-	std::vector<Player*> player = {new Player("Player_1", 1300), new Player("Player_2", 1440), new Player("Player_3", 1220), new Player("Player_4", 1001)};
 
-	GameBoard gameboard = GameBoard(player);	
+	constexpr int MAX_ROUND = 20;
 	
-	for(int i=0; i<400; i++)
+	std::vector<Player*> player = {new Player("Player_1", 100), new Player("Player_2", 100), new Player("Player_3", 100), new Player("Player_4", 100)};
+
+	GameBoard gameboard = GameBoard(player);
+	
+	std::vector<int> order = player_order();
+	
+	std::string arg (argv[1]);
+	
+	if(argc < 2 || arg == "computer")
 	{
-		for(int j=0; j<4; j++)
+		for(int i=0; i<MAX_ROUND; i++)
 		{
-			play_bot(gameboard, player.at(j));
+			for(int j=0; j<player.size(); j++)
+			{
+				if(player.at(order.at(j))->is_in_game_())
+					play_bot(gameboard, player.at(order.at(j)));
+					
+				std::system("clear");
+				gameboard.show();
+				usleep(305000);
+			}
+			
+			std::system("clear");
+			gameboard.show();
+			usleep(15000);
+			
+			cout << "\033[1;31mBot Win!\033[0m\n";
 		}
-		
-		std::system("clear");
-		gameboard.show();
-		usleep(180000);
+	}
+	
+	if(arg == "human")
+	{
+		for(int i=0; i<MAX_ROUND; i++)
+		{
+			std::system("clear");
+			gameboard.show_gameboard();
+			usleep(15000);
+			
+			cout << endl;
+				
+			if(player.at(order.at(0))->is_in_game_())
+					play_human(gameboard, player.at(order.at(0)));
+					
+			for(int j=1; j<player.size(); j++)
+			{
+				if(player.at(order.at(j))->is_in_game_())
+					play_bot(gameboard, player.at(order.at(j)));
+					
+				std::system("clear");
+				gameboard.show_gameboard();
+				usleep(305000);
+			}
+			
+			std::system("clear");
+			gameboard.show_gameboard();
+			usleep(15000);
+		}
+	}
+	
+	//destroyer of player pointer
+	for(int i=0; i<4; i++)
+	{
+		delete player.at(i);
 	}
 	
 	return 0;
